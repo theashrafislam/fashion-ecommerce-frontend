6647e15d2125a2030c7d4beb470f0ff5cbcf11d9
Ashraful Islam <theashrafislam@gmail.com>
2025-01-26 11:18:13 +0600

    footer components all work done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import { FaFacebookF, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { IoLogoLinkedin } from 'react-icons/io';
 
 const Footer = () => {
     return (
         <section className='font-secondary'>
             {/* first section  */}
             <div className='bg-[#F6F7F7] '>
-                <div className='flex justify-between gap-5 max-w-7xl mx-auto py-12'>
+                <div className='flex flex-col px-5 md:px-3 lg:px-0 md:flex-row justify-between gap-5 max-w-7xl mx-auto py-12'>
                     {/* contact us  */}
                     
-                    <div className='w-[30%]'>
+                    <div className='md:w-[30%]'>
                         <h4 className='mb-7 font-primary text-xl text-[#2E2E2E]'>Contact Us</h4>
                         <p className='mb-4 font-secondary text-sm text-[#535353]'>We'd love to hear from you! Feel free to reach out through any of the following methods.</p>
                         <p className='mb-1 font-secondary text-sm text-[#535353]'><strong className='text-[#666666]'>Address:</strong> 123 Main Street, Anytown, CA 12345, USA.</p>
                         <p className='mb-1 font-secondary text-sm text-[#535353]'><strong className='text-[#666666]'>Call to:</strong> (012) 800 456 789-987</p>
                         <p className='font-secondary text-sm text-[#535353]'><strong className='text-[#666666]'>Mail to:</strong> theashrafislam@gmail.com</p>
+                        {/* icons */}
+                        <div className='flex items-center gap-3 mt-3'>
+                            <a target='_blank' href="https://www.facebook.com/theashrafislam/"><FaFacebookF className='text-xl hover:text-red-500'/></a>
+                            <a target='_blank' href="https://www.instagram.com/theashrafislam/"><FaInstagram className='text-xl hover:text-red-500'/></a>
+                            <a target='_blank' href="https://www.linkedin.com/in/theashrafislam/"><IoLogoLinkedin className='text-xl hover:text-red-500'/></a>
+                            <a target='_blank' href="https://github.com/theashrafislam"><FaGithub className='text-xl hover:text-red-500'/></a>
+                        </div>
                     </div>
                     {/* information  */}
-                    <div className='w-[20%] pl-5'>
+                    <div className='md:w-[20%] md:pl-5'>
                         <h4 className='text-xl font-primary text-[#2E2E2E] mb-7'>Information</h4>
                         <div className='flex flex-col gap-2 text-sm text-[#666666]'>
                             <a href="#" className='hover:text-red-500 hover:translate-x-1 transition duration-300'>About Us</a>
@@ -28,7 +37,7 @@ const Footer = () => {
                         </div>
                     </div>
                     {/* my account  */}
-                    <div className='w-[20%] pl-5'>
+                    <div className='md:w-[20%] md:pl-5'>
                         <h4 className='text-xl text-[#2E2E2E] mb-7 font-primary'>My Account</h4>
                         <div className='flex flex-col gap-2 text-sm text-[#666666]'>
                             <a href="#" className='hover:text-red-500 hover:translate-x-1 transition duration-300'>My Account</a>
@@ -40,7 +49,7 @@ const Footer = () => {
                         </div>
                     </div>
                     {/* Newsletter */}
-                    <div className='w-[30%]'>
+                    <div className='md:w-[30%]'>
                         <h4 className='text-xl text-[#2E2E2E] mb-7 font-primary'>Newsletter</h4>
                         <p className='mb-4 font-secondary text-sm text-[#535353]'>Get E-mail updates about our latest shop and special offers.</p>
                         <input type="email" name="" id="" placeholder='Enter you email here.' className='border-b-1 w-full mb-5 p-2 text-base focus:outline-none focus:border-b-red-500 text-[#666666]' /> <br />
