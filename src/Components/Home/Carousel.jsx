c04753682d717d9b4e6970bcb70cfc218b20cd8a
Ashraful Islam <theashrafislam@gmail.com>
2025-02-13 00:23:58 +0600

    carousel components is complate with good photo

--- src/Components/Home/Carousel.jsx
+++ src/Components/Home/Carousel.jsx
@@ -7,9 +7,12 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 // import required modules
-import { Pagination, Navigation } from 'swiper/modules';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 
 const Carousel = () => {
+
+    const img = [1, 2, 3, 4, 5, 6, 7];
+
     return (
         <>
             <Swiper
@@ -20,44 +23,34 @@ const Carousel = () => {
                     clickable: true,
                 }}
                 navigation={true}
-                modules={[Pagination, Navigation]}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}
+                modules={[Pagination, Navigation, Autoplay]}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <div
-                        className="w-full h-[600px] bg-cover bg-center flex items-center text-white"
-                        style={{ backgroundImage: "url('/carousel/slide-1-2.jpg')" }}
-                    >
-                        <div className='flex flex-col md:flex-col justify-start px-40'>
-                            <div className='w-[50%]'>
-                                <h1 className='font-primary font-bold text-7xl'><span className='text-nowrap'>Women New</span> Collection</h1>
+                {
+                    img?.map((slide) => (
+                        <SwiperSlide>
+                            <div
+                                key={slide}
+                                className="w-full h-[700px] bg-cover bg-center flex items-center text-white"
+                                style={{ backgroundImage: `url('/carousel/${slide}.jpg')` }}
+                            >
+                                <div className='flex flex-col md:flex-col justify-start px-40'>
+                                    <div className='w-[50%]'>
+                                        <h1 className='font-primary font-bold text-7xl text-[#2e2e2e]'><span className='text-nowrap'>Women New</span> Collection</h1>
+                                        <p className='text-[#2e2e2e] py-2 text-lg font-secondary'>Up to 70% off selected Product</p>
+                                        <button
+                                            className='w-full mt-7 md:w-fit px-10 py-3 hover:bg-black rounded-md text-white bg-red-500 hover:text-white font-semibold font-primary'>Shop Now</button>
+                                    </div>
+                                    <div className='w-[50%'></div>
+                                </div>
                             </div>
-                            <div className='w-[50%'></div>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div
-                        className="w-full h-[800px] bg-cover bg-center flex items-center justify-center text-white"
-                        style={{ backgroundImage: "url('/carousel/slide-1-2.jpg')" }}
-                    >
-                        <h1 className="text-3xl font-bold bg-black bg-opacity-50 p-4 rounded-lg">
-                            Hello, I am Ashraful Islam
-                        </h1>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div
-                        className="w-full h-[800px] bg-cover bg-center flex items-center justify-center text-white"
-                        style={{ backgroundImage: "url('/carousel/slide-1-2.jpg')" }}
-                    >
-                        <h1 className="text-3xl font-bold bg-black bg-opacity-50 p-4 rounded-lg">
-                            Hello, I am Ashraful Islam
-                        </h1>
-                    </div>
-                </SwiperSlide>
-
-
+                        </SwiperSlide>
+                    ))
+                }
             </Swiper>
         </>
     );
