2940e856c51fc1a1b1749a28f2e2f220a63ab160
Ashraful Islam <theashrafislam@gmail.com>
2025-02-19 17:04:09 +0600

    complate contact fourm page desgin

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ActivePage from '../Components/ActivePage';
+import { FaLocationDot } from "react-icons/fa6";
+import { FaMobileAlt } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 const Contact = () => {
     return (
@@ -41,6 +44,7 @@ const Contact = () => {
                             />
                         </div>
                         <textarea
+                            rows="8"
                             placeholder="Enter your message"
                             className="mt-1 block w-full p-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-red-500 focus:border-red-500"
                         >
@@ -57,6 +61,35 @@ const Contact = () => {
                 {/* Contact Inf */}
                 <div className='w-full md:w-[30%]'>
                     <h1 className='pb-4 border-b-2 border-[rgb(235,235,235)] text-3xl text-[#2E2E2E]'>Contact Info</h1>
+                    <div className='my-10 space-y-6'>
+                        {/* Postal Address */}
+                        <div className='flex gap-4'>
+                            <FaLocationDot className='text-2xl mt-1' />
+                            <div>
+                                <h3 className='text-lg text-[#212121] mb-1'>Postal Address</h3>
+                                <p className='font-secondary text-sm text-[#666666]'>PO Box 123456, Street/Road <br />
+                                    Country-State</p>
+                            </div>
+                        </div>
+                        {/* Contact Us Anytime */}
+                        <div className='flex gap-4'>
+                            <FaMobileAlt className='text-2xl mt-1' />
+                            <div>
+                                <h3 className='text-lg text-[#212121] mb-1'>Contact Us Anytime</h3>
+                                <p className='font-secondary text-sm text-[#666666]'>Mobile: 012 345 678 <br />
+                                    Fax: 123 456 789</p>
+                            </div>
+                        </div>
+                        {/* Support Overall  */}
+                        <div className='flex gap-4'>
+                            <MdEmail className='text-2xl mt-1' />
+                            <div>
+                                <h3 className='text-lg text-[#212121] mb-1'>Support Overall</h3>
+                                <p className='font-secondary text-sm text-[#666666]'>Support24/7@example.com <br />
+                                    <span className='hover:text-red-500 hover:cursor-pointer'>info@example.com</span></p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
