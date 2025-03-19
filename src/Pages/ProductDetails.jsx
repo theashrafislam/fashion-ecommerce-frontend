f26e8d0643f57cd9025d9dbdae7ea794357c9e8f
Ashraful Islam <theashrafislam@gmail.com>
2025-03-19 10:19:04 +0600

    try to make responsive product details page

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -10,10 +10,10 @@ const ProductDetails = () => {
             <ActivePage route={'Product Details'} mainRoute={'Home'} />
             {/* main section  */}
             <div className='max-w-7xl mx-auto my-14 flex flex-col items-start justify-between md:flex-row gap-5'>
-                <div className='w-[40%]'>
+                <div className='w-full lg:w-[40%]'>
                     hello i am image
                 </div>
-                <div className='w-[60%]'>
+                <div className='w-full lg:w-[60%]'>
                     hello i am details
                 </div>
             </div>
