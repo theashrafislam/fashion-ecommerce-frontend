f07614071479934850be029706d2e3d1f5b92037
Ashraful Islam <theashrafislam@gmail.com>
2025-04-05 08:38:19 +0600

    rewrite user auth slice function

--- src/Features/Auth/useAuthSlice.js
+++ src/Features/Auth/useAuthSlice.js
@@ -1,24 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: null,
-  loading: true,
+    user: null,
+    loading: true
 };
 
 const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    setUser(state, action) {
-      state.user = action.payload;
-      state.loading = false;
-    },
-    clearUser(state) {
-      state.user = null;
-      state.loading = false;
-    },
-  },
+    name: 'auth',
+    initialState,
+    reducers: {
+        setUser(state, action) {
+            state.user = action.payload;
+            state.loading = false;
+        },
+        clearUser(state, action) {
+            state.user = null;
+            state.loading = false;
+        }
+    }
 });
 
-export const { setUser, clearUser } = authSlice.actions;
+export const {setUser, clearUser} = authSlice.actions;
 export default authSlice.reducer;
